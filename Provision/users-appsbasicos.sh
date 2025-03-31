545a19062dc22ea5747087a11ef5b5c460f934b2
Fernando Dias <ferpaesdias@gmail.com>
2025-03-31 15:40:06 -0300

    Adição de entrada do arquivo /etc/hosts

--- Provision/users-appsbasicos.sh
+++ Provision/users-appsbasicos.sh
@@ -4,13 +4,16 @@
 PASS='control123'
 
 # Usuários  
-function addUser {
+function add_User {
 
     # Ajuste fuso horario
     sudo timedatectl set-timezone America/Sao_Paulo
-  
-    # Alterar a senha do usuário "vagrant" 
-    sudo usermod -p $(openssl passwd -1 ${PASS}) vagrant
+
+# Adiciona uma entrada do Node1 no Hosts dos Nodes
+cat <<-EOF | sudo tee -a /etc/hosts
+192.168.3.201 k8s-control-plane
+EOF
+
 
     # Alterar a senha do usuário "root" 
     sudo usermod -p $(openssl passwd -1 ${PASS}) root 
@@ -18,7 +21,7 @@ function addUser {
 
 # Instalar aplicativos iniciais
 function installApp {
-    
+
     sudo apt update > /dev/null 2>&1 && sudo apt upgrade -y > /dev/null 2>&1
-    sudo apt install -y vim curl wget apt-transport-https ca-certificates curl gpg gnupg lsb-release git
+    sudo apt install -y vim curl wget apt-transport-https ca-certificates curl gpg gnupg lsb-release git > /dev/null 2>&1
 }
